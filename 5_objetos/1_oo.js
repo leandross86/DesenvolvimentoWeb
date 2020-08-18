29522bebe3535b6364525249f2f20a8296b72c24
Leandross86 <leandrobko@gmail.com>
2020-08-17 22:19:11 -0300

    continuando

---
+++ 5_objetos/1_oo.js
@@ -0,0 +1,24 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO(Orientação a Objetos)
+objeto = {
+  valor1,
+  valor2,
+  valor3,
+  valor4,
+  processamento() {
+    // ...
+  }
+}
+
+objeto.processamento()  // Foco passou a ser o objeto
+
+// PRINCIPIOS IMPORTANTES:
+
+// 1. Abstração
+// 2. Encapsulamento
+// 3. herança (Prototype)
+// 4. Polimorfismo -> Multiplaformas
